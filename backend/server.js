2efb44d7632247d4e95c0de1b27132f6c7317649
Manthan vaghasiya <manthanvaghasiya60@gmail.com>
2026-03-07 21:36:25 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(cors({
   // Put your exact Vercel URL in the quotes below!
-  origin: ["http://localhost:5173", "https://your-vercel-app-name.vercel.app"], 
+  origin: ["http://localhost:5173", "https://arcelorapiportal.vercel.app/"], 
   credentials: true
 }));
 // Tell the messenger to use our new auth paths!
